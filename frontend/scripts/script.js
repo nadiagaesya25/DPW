8d2e25002a7e2bd2659cff7138f5486a02c36f8d
nadiagaesya25 <nadia.gaesya1650@student.unri.ac.id>
2022-03-14 17:29:56 +0700

    first commit

---
+++ frontend/scripts/script.js
@@ -0,0 +1,7 @@
+const h1 = document.getElementById("hai") //getElementById digunakan untuk Mendapatkan elemen dengan yang ditentukan ID
+h1.style.color = "green"; //digunakan untuk memberi warna pada kalimat
+h1.style.backgroundColor = "black"; //digunakan untuk memberi warna background tulisan
+
+const h1 = document.getElementByTagName("p");
+h2[0].style.color = "blue";
+h2[0].style.backgroundColor = "black";
